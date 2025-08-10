f2747d278890c30c5a7244fc5b9eb9ea58fc7588
Pinwestycji <damisamis85@gmail.com>
2025-08-10 11:17:09 +0200

    Update chart.js

--- chart.js
+++ chart.js
@@ -138,5 +138,5 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     // Załaduj przykładowy wykres na starcie (opcjonalnie)
-    loadChartData('CDR');
+    loadChartData('CDPROJEKT');
 });
