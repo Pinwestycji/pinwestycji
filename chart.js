bb857e162a0f4cbf189c16bbf3eeff9de006e2ba
Pinwestycji <damisamis85@gmail.com>
2025-08-12 18:03:48 +0200

    Update chart.js
    change : https://pinwestycji.onrender.com

--- chart.js
+++ chart.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     // URL twojego API na Render.com
-    const API_URL = 'https://pinwestycji.pythonanywhere.com/';
+    const API_URL = 'https://pinwestycji.onrender.com';
 
     // Inicjalizacja wykresu
     const chartContainer = document.getElementById('tvchart');
