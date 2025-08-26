5fc3014285e3bfa17cd725d329039a49a6a6a46b
Pinwestycji <damisamis85@gmail.com>
2025-08-26 14:26:15 +0200

    Update chart.js
    projectionChartContainer

--- chart.js
+++ chart.js
@@ -148,7 +148,7 @@ document.addEventListener('DOMContentLoaded', function() {
                  // Ponowna inicjalizacja wykresu prognoz, ponieważ odtworzyliśmy jego kontener
                  projectionChart.remove(); // Usuwamy stary wykres
                 
-                 const newProjectionChart = LightweightCharts.createChart(newProjectionChart, { width: newProjectionChart.clientWidth, height: 300, layout: { backgroundColor: '#ffffff', textColor: '#333' }, grid: { vertLines: { color: '#f0f0f0' }, horzLines: { color: '#f0f0f0' } } });
+                 const newProjectionChart = LightweightCharts.createChart(projectionChartContainer, { width: projectionChartContainer.clientWidth, height: 300, layout: { backgroundColor: '#ffffff', textColor: '#333' }, grid: { vertLines: { color: '#f0f0f0' }, horzLines: { color: '#f0f0f0' } } });
                  const newProjectionSeries =  newProjectionChart.addSeries(LightweightCharts.HistogramSeries);
                  newProjectionSeries.applyOptions({
                     color: 'rgba(33, 150, 243, 0.8)'
