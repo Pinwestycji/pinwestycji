4a3ab4393001bf0ff08e379d27a83fac69861295
Pinwestycji <damisamis85@gmail.com>
2025-10-03 18:16:35 +0200

    Update chart.js
    function handleMouseUp(e) {
            if (isDragging) {
                isDragging = false;
                draggedHandleIndex = null;
                // Przywróć normalną interakcję z wykresem
                mainChart.applyOptions({ handleScroll: true, handleScale: true });
            }
            // Po puszczeniu myszy, stan się nie zmienia (kształt wciąż jest zaznaczony)
            // ale na wszelki wypadek można tu zostawić wywołanie
      //      updateCanvasPointerEvents();
        }

--- chart.js
+++ chart.js
@@ -656,7 +656,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }
         // Po puszczeniu myszy, stan się nie zmienia (kształt wciąż jest zaznaczony)
         // ale na wszelki wypadek można tu zostawić wywołanie
-        updateCanvasPointerEvents();
+  //      updateCanvasPointerEvents();
     }
     
     animationLoop();
